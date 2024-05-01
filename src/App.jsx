36f213e40525ff73b2ebc91f8b99433c1da9fd3a
Valerii <valeraviachalo@gmail.com>
2024-05-01 18:09:11 +0300

    develop branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import { useLocation, useRoutes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query";
 import { AnimatePresence } from "framer-motion";
 
 import { ScrollProvider } from "./helpers/scrollProvider";
-import { Header } from "@C/Header/Header"
+import { Header } from "@C/Header/Header";
 import Home from "./pages/Home/Home";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
-import Blog from "./pages/Blog/Blog";
-import BlogDetails from "./pages/BlogDetails/BlogDetails";
 import Footer from "./components/Footer/Footer";
 import { Loader } from "./components/Loader/Loader";
 import classNames from "classnames";
@@ -16,7 +14,7 @@ import classNames from "classnames";
 const queryC = new QueryClient();
 
 function App() {
-  const [loaderFinished, setLoaderFinished] = useState(false);
+  const [loaderFinished, setLoaderFinished] = useState(true); // TODO: CHANGE IT TO FALSE
 
   const element = useRoutes([
     {
@@ -26,19 +24,6 @@ function App() {
           index: true,
           element: <Home />,
         },
-        {
-          path: 'blog',
-          element: <Blog />,
-        },
-        {
-          path: 'blogs',
-          children: [
-            {
-              path: ":blogId?",
-              element: <BlogDetails />,
-            },
-          ],
-        }
       ],
     },
     {
@@ -51,20 +36,20 @@ function App() {
 
   return (
     <QueryClientProvider client={queryC}>
-      <main className={classNames("main", {
-        "main--loading": !loaderFinished
-      })}>
-          <ScrollProvider>
-            {!loaderFinished && (<Loader setLoaderFinished={setLoaderFinished}/>)}
-            <Header />
-            <AnimatePresence mode="wait" initial={false}>
-                {React.cloneElement(element, { key: location.pathname })}
-            </AnimatePresence>
-            <Footer />
-          </ScrollProvider>
+      <main
+        className={classNames("main", {
+          "main--loading": !loaderFinished,
+        })}
+      >
+        <ScrollProvider>
+          {!loaderFinished && <Loader setLoaderFinished={setLoaderFinished} />}
+          <AnimatePresence mode="wait" initial={false}>
+            {React.cloneElement(element, { key: location.pathname })}
+          </AnimatePresence>
+        </ScrollProvider>
       </main>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,17 +3,13 @@ import { Link } from 'react-router-dom';
 import FullWidthBg from '@/components/FullWidthBg/FullWidthBg'
 
 import './ErrorPage.scss';
-import { Transition } from '@/components/Transition/Transition';
-import { useIsPresent } from 'framer-motion';
 
 export default function ErrorPage() {
-  const isPresent = useIsPresent();
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
 
   return (
-    <>
     <FullWidthBg
       type="video"
       url="https://images.beta.cosmos.so/17de808f-e3ab-46bd-82f4-cde43d7ec60b.mp4"
@@ -28,8 +24,5 @@ export default function ErrorPage() {
         </h1>
       </div>
     </FullWidthBg>
-
-    <Transition isPresent={isPresent} />
-    </>
   )
 }
