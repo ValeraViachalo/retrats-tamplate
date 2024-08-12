a4c8e6595a2b67ff7e4d8c03b98d194da8b612f4
Valerii <valeraviachalo@gmail.com>
2024-08-12 19:48:14 +0300

    anim function

--- src/helpers/anim.js
+++ src/helpers/anim.js
@@ -1,3 +1,12 @@
+export const anim = (variants) => {
+  return {
+    initial: "initial",
+    animate: "animate",
+    exit: "exit",
+    variants,
+  };
+};
+
 export const Menu = {
   menuOpen: {
     initial: {
